6d909061c2187e5eefbe7a215c028a78a93eb159
Wintry <FreezyColds@gmail.com>
2019-03-03 02:59:35 -0700

    Add files via upload

---
+++ discord.js/info.js
@@ -0,0 +1,10 @@
+module.exports = {
+	Prefix: ".",
+	Token: "NTUxNjA3NTk1OTk2MTUxODM4.D1zcSg.EdHDc0KpXTv5ly5f7selQiyUPRs",
+	MainColor: 0x16b3ff,
+
+	Colors: {
+		"really red": 0xFF0000,
+		"toothpaste": 0x16b3ff,
+	}
+}
